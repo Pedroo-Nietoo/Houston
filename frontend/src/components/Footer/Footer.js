5d87ba8d146bc7093efe326c7b2126432e462651
Pedro Nieto <pedronieto.2005@gmail.com>
2023-09-25 16:43:44 -0300

    wip: editing landing page

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,8 +1,68 @@
+import React, { useEffect } from 'react';
 import './Footer.css';
 
 export default function Footer() {
+    const handleSmoothScroll = (e) => {
+        const href = e.target.getAttribute('href');
+        if (href && href.startsWith('#')) {
+            e.preventDefault();
+            const targetId = href.substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop,
+                    behavior: 'smooth',
+                });
+            }
+        }
+    };
+
+    useEffect(() => {
+        const links = document.querySelectorAll('.Footer a');
+        links.forEach((link) => {
+            link.addEventListener('click', handleSmoothScroll);
+        });
+
+        // Remova os ouvintes de evento quando o componente é desmontado
+        return () => {
+            links.forEach((link) => {
+                link.removeEventListener('click', handleSmoothScroll);
+            });
+        };
+    }, []);
+
     return (
-        <div className='Footer'>
-        </div>
+        <footer className='Footer'>
+            <div className='items'>
+                <ul>
+                    <h3>Início</h3>
+                    <li><a href='#home'>Home</a></li>
+                    <li><a href='#features'>Features</a></li>
+                    <li>Features</li>
+                </ul>
+                <ul>
+                    <h3>Sobre nós</h3>
+                    <li>Informações da Empresa</li>
+                    <li><a href='mailto:pedronieto.2005@gmail.com?subject=Suporte - Houston' target='_blank' rel="noreferrer">Contato</a></li>
+                    <li>Copyright</li>
+                </ul>
+                <ul>
+                    <h3>Termos de Serviço</h3>
+                    <li><a href='http://localhost:3000/termsOfService' target='_blank' rel="noreferrer">Termos Gerais</a></li>
+                    <li><a href='http://localhost:3000/termsOfService' target='_blank' rel="noreferrer">Termos de Uso</a></li>
+                    <li><a href='http://localhost:3000/termsOfService' target='_blank' rel="noreferrer">Política de Privacidade</a></li>
+                </ul>
+                <ul>
+                    <h3>Redes sociais</h3>
+                    <li><a href='https://www.linkedin.com/in/pedroo-nietoo/' target='_blank' rel="noreferrer">LinkedIn</a></li>
+                    <li><a href='https://github.com/Pedroo-Nietoo' target='_blank' rel="noreferrer">GitHub</a></li>
+                    <li><a href='https://www.instagram.com/pedroo_nietoo' target='_blank' rel="noreferrer">Instagram</a></li>
+                </ul>
+            </div>
+            <div className='copyright'>
+                <p>© 2023 Copyright - Houston</p>
+            </div>
+        </footer>
     );
 }
--- frontend/src/pages/LandingPage/LandingPage.js
+++ frontend/src/pages/LandingPage/LandingPage.js
@@ -1,17 +1,17 @@
 import './LandingPage.css';
 import { Link } from 'react-router-dom';
-import templateImg from '../../../public/assets/images/template.png';
+import templateImg from '../../assets/images/template.png';
 import Feature from '../../components/Feature/Feature';
-import habitFeature from '../../../public/assets/images/habitFeature.png';
-import customizationFeature from '../../../public/assets/images/customizationFeature.png';
-import tasksFeature from '../../../public/assets/images/tasksFeature.png';
-import dashboardFeature from '../../../public/assets/images/dashboardFeature.png';
+import habitFeature from '../../assets/images/habitFeature.png';
+import customizationFeature from '../../assets/images/customizationFeature.png';
+import tasksFeature from '../../assets/images/tasksFeature.png';
+import dashboardFeature from '../../assets/images/dashboardFeature.png';
 import Footer from '../../components/Footer/Footer';
 
 export default function LandingPage() {
     return (
         <div>
-            <header>
+            <header id="home">
                 <nav className='navbar'>
                     <ul className='navItems'>
                         <div>
@@ -41,10 +41,12 @@ export default function LandingPage() {
                 <img className='template' src={templateImg} alt='app' />
             </header>
             <main>
-                <Feature imgURL={habitFeature} title="Habit Tracking" content="Crie hábitos, marque como realizados, customize-os e acompanhe seu progresso." />
-                <Feature imgURL={customizationFeature} title="Personalização" content="Aplicativo com cores personalizáveis e light/dark mode para ficar do seu agrado." />
-                <Feature imgURL={tasksFeature} title="Lista de tarefas" content="Controle sueus afazeres utilizando o sistema de lista de tarefas." />
-                <Feature imgURL={dashboardFeature} title="Gráficos de progresso" content="Acompanhe o progresso de seus hábitos com Dashboards incríveis." />
+                <div id="features">
+                    <Feature imgURL={habitFeature} title="Habit Tracking" content="Crie hábitos, marque como realizados, customize-os e acompanhe seu progresso." />
+                    <Feature imgURL={customizationFeature} title="Personalização" content="Aplicativo com cores personalizáveis e light/dark mode para ficar do seu agrado." />
+                    <Feature imgURL={tasksFeature} title="Lista de tarefas" content="Controle sueus afazeres utilizando o sistema de lista de tarefas." />
+                    <Feature imgURL={dashboardFeature} title="Gráficos de progresso" content="Acompanhe o progresso de seus hábitos com Dashboards incríveis." />
+                </div>
             </main>
             <Footer />
         </div>
